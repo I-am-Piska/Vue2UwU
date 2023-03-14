2ce60661ddb2da83d7480d1845d7f5e7debb06e3
I-am-Piska <doctor.who.rip@gmail.com>
2023-03-14 13:23:36 +0700

    туктук

--- js/main.js
+++ js/main.js
@@ -71,6 +71,133 @@ Vue.component('component', {
     }
 })
 
+Vue.component('newCard', {
+    template: `
+    <section id="main" class="main-alt">
+    
+        <form @submit.prevent="Submit">
+        
+        <div class="text">
+            <h2>Запишите что нибудь</h2>
+        </div>    
+            
+        <div class="form_control">
+                
+            <div class="form_name">
+                <input required type="text" v-model="name" id="name" placeholder="Введите название заметки"/>
+            </div>
+            
+            <input required type="text"  v-model="point_1" placeholder="Первый"/>
+            <br>
+            <input required type="text"  v-model="point_2" placeholder="Второй"/>
+            <br>
+            <input required type="text"  v-model="point_3" placeholder="Третий"/> 
+            <br>
+            <input  type="text"  v-model="point_4"  placeholder="Четвертый"/>
+            <br>
+             <input type="text" v-model="point_5"  placeholder="Пятый"/>
+        </div>
+            <div class="form_control">
+                <button class="btn">Отправьте что нибудь</button>
+            </div>
+        </form>
+    </section>
+    `,
+    data() {
+        return {
+            name: null,
+            point_1: null,
+            point_2: null,
+            point_3: null,
+            point_4: null,
+            point_5: null,
+            date: null,
+        }
+    },
+    methods: {
+
+        Submit() {
+            let card = {
+                name: this.name,
+                points: [
+                    {name: this.point_1, completed: false},
+                    {name: this.point_2, completed: false},
+                    {name: this.point_3, completed: false},
+                    {name: this.point_4, completed: false},
+                    {name: this.point_5, completed: false}
+                ],
+                date: null,
+                status: 0,
+                errors: [],
+            }
+            eventBus.$emit('addColumn_1', card)
+            this.name = null;
+            this.point_1 = null
+            this.point_2 = null
+            this.point_3 = null
+            this.point_4 = null
+            this.point_5 = null
+        }
+    }
+
+})
+
+Vue.component('column_1', {
+    template: `
+        <section id="main" class="main-alt">
+            <div class="column 1">
+            <p>Начало</p>
+            
+                <div v-for="card in column_1">
+                <h3>{{ card.name }}</h3>
+                    <ul v-for="task in card.points"
+                        v-if="task.name != null"
+                        @click="TaskCompleted(card, task)"
+                        :class="{completed: task.completed}">
+                        <p>
+                        {{ task.name }}
+                        </p>
+                    </ul>
+                    
+                </div>
+            </div>
+        </section>
+    `,
+    props: {
+        column_1: {
+            type: Array,
+        },
+        column_2: {
+            type: Array,
+        },
+        card: {
+            type: Object,
+        },
+        errors: {
+            type: Array,
+        },
+    },
+    methods: {
+        TaskCompleted(ColumnCard, task) {
+            if (task.completed === false){
+                task.completed = true
+                ColumnCard.status += 1
+            }
+            let count = 0
+            for(let i = 0; i < 5; i++){
+                if(ColumnCard.points[i].name !== null){
+                    count++
+                }
+            }
+            if ((ColumnCard.status / count) * 100 >= 50) {
+                eventBus.$emit('addColumn_2', ColumnCard)
+                this.column_1.splice(this.column_1.indexOf(ColumnCard), 0)
+            }
+        },
+    },
+})
+
+
 
 
 let app = new Vue({
